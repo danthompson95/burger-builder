52c38e220e1e0c18c464d3cdcbdbdd16e9a1dbfb
Daniel Thompson <18499520+danthompson95@users.noreply.github.com>
2020-08-12 11:46:16 +0200

    Initial Commit
    Post-deploy progress
    
    https://dt95-burger-builder.web.app/

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# burger-builder
+#Burger Builder
+
+Built with create-react-app
+npm install
+
+This project was built following a udemy course but covers React, Redux and Routing.
+Firebase config, firebase authentication.
---
+++ src/components/Order/Order.js
@@ -0,0 +1,44 @@
+import React from "react"
+
+import classes from "./Order.css"
+
+const Order = (props) => {
+  const ingredients = []
+
+  for (let ingredientName in props.ingredients) {
+    if (props.ingredients[ingredientName] > 0) {
+      ingredients.push({
+        name: ingredientName,
+        amount: props.ingredients[ingredientName]
+      })
+    }
+  }
+
+  const ingredientOutput = ingredients.map((ig) => {
+    return (
+      <span
+        key={ig.name}
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px"
+        }}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    )
+  })
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>
+        Price: <strong>£{props.price.toFixed(2)}</strong>
+      </p>
+    </div>
+  )
+}
+
+export default Order
